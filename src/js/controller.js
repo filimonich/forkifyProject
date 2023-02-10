78bf482d494af005e01ab97629c492786801de23
Sergey Filimonov <srfilimonoff@gmail.com>
2023-02-10 13:17:24 +0300

    loading a recipe from api

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,4 +11,35 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
-console.log(12 + 38 + 1 + 14); // 09.02.23
+// console.log(12 + 38 + 1 + 14); // 09.02.23
+console.log(24 + 22); // 10.02.23
+
+const showRecipe = async function () {
+  try {
+    // Ajax запрос к API
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+
+    // if ok: false
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    // Так как есть recipe с обеих сторон, можно использовать Деструктурирующее присваивание { recipe }
+    // let recipe = data.data.recipe;
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publesher,
+      sourceUrl: recipe.source_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+showRecipe();
