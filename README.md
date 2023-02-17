2c04dd30ff91da35935c5a4a8f31ddcd28b0156d
Sergey Filimonov <srfilimonoff@gmail.com>
2023-02-17 12:52:34 +0300

    add a description to the readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# forkifyProject
+# Forkify Project
+
+Recipe application with custom recipe uploads.
+Приложение для рецептов с возможностью загрузки пользовательских рецептов.
--- src/js/config.js
+++ src/js/config.js
@@ -3,3 +3,6 @@ export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = 'ad5ef4f7-caf5-4ebe-9bc7-c9bef4995f27';
 export const MODAL_CLOSE_SEC = 2.5;
+
+///////////////////////////////////////
+console.log(10); // 17.02.23
